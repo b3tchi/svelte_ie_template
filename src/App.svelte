7d6e94cd22490f1c4e6919406cec744dc130b870
Jan Bečka/BDS <janbecka@LEGO.com>
2020-05-12 21:36:02 +0200

    svelte minor update

--- src/App.svelte
+++ src/App.svelte
@@ -9,24 +9,47 @@
 	
     async function getRandomQuote() {
         gettingQuote = true;
+        
+        
         var dte = Date.now();
-        const res = await fetch("https://api.quotable.io/random?source=" + dte);
-        const resP = await fetch('https://dog.ceo/api/breeds/image/random');
+        
+        const res = await fetch('http://localhost:8001/query?source=' + dte
+            , {
+                headers: {
+                    'Accept': 'application/json'
+                    , 'Content-Type': 'application/json'
+                
+                }
+                , method: 'POST'
+                , body: JSON.stringify({a: 1, b: 2})
+            }
+        );
+        // const data = await res.json();
+        // if (res.ok) {
+        //     return data;
+        // }else{
+        //     throw new Error(data);
+        // }
+        
+        
+        // const res = await fetch("https://api.quotable.io/random?source=" + dte);
+        // const resP = await fetch('https://dog.ceo/api/breeds/image/random');
         
         const json = await res.json();
-        const jsonP = await resP.json();
+        // const jsonP = await resP.json();
 
         console.log(json)
-        console.log(jsonP)
+        // console.log(jsonP)
 
-        non = [...non, json];
+        // non = [...non, json]; Append JSON    
+        non = json;
         gettingQuote = false;
     }
 </script>
 
 <div>
-    {#each non as {author, content}}
-        <p>{content} - {author}</p>
+    {#each non as {ID , Field1}}
+        <p>{ID} - {Field1}</p>
    	{/each}
 		<button on:click={getRandomQuote} disabled={gettingQuote}>Get Quote</button>
     {#if gettingQuote}
